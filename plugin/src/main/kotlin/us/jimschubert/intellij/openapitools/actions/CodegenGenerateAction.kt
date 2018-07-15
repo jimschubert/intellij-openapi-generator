78e1ac45f33d333aa6d56a4c7de4dc54da7f8871
Jim Schubert <james.schubert@gmail.com>
2018-07-15 17:36:01 -0400

    Move package and copyright back to us.jimschubert

---
+++ plugin/src/main/kotlin/us/jimschubert/intellij/openapitools/actions/CodegenGenerateAction.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 Jim Schubert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package us.jimschubert.intellij.openapitools.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import us.jimschubert.intellij.openapitools.events.GenerationNotificationManager
+import us.jimschubert.intellij.openapitools.ui.GenerateDialog
+
+class CodegenGenerateAction : AnAction() {
+
+    private val notificationManager: us.jimschubert.intellij.openapitools.events.GenerationNotificationManager = us.jimschubert.intellij.openapitools.events.GenerationNotificationManager()
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val file = e.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return
+
+        us.jimschubert.intellij.openapitools.ui.GenerateDialog(project, file, notificationManager).show()
+    }
+
+    override fun update(e: AnActionEvent?) {
+        try {
+            val file = e?.getData(PlatformDataKeys.VIRTUAL_FILE)
+            val ext = file?.extension?.toLowerCase() ?: ""
+            if (e != null && file != null) {
+                // TODO: Add a better condition here, or maybe support user-defined regex
+                e.presentation.isEnabled = validExtensions.contains(ext)
+            } else {
+                super.update(e)
+            }
+        } catch(ex: Throwable) {
+            super.update(e)
+        }
+    }
+
+    companion object {
+        val validExtensions = listOf("yaml", "yml", "json")
+    }
+}
---
+++ plugin/src/main/kotlin/us/jimschubert/intellij/openapitools/ui/LogoLabel.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 Jim Schubert
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package us.jimschubert.intellij.openapitools.ui
+
+import com.intellij.openapi.util.IconLoader
+import com.intellij.ui.components.JBLabel
+import com.intellij.util.ui.UIUtil
+import javax.swing.Icon
+
+class LogoLabel : JBLabel() {
+
+    /**
+     * Returns the graphic image (glyph, icon) that the label displays.
+     *
+     * @return an Icon
+     * @see .setIcon
+     */
+    override fun getIcon(): Icon {
+        return when {
+            UIUtil.isUnderDarcula() -> dark
+            else -> light
+        }
+    }
+
+    companion object {
+        private val light = IconLoader.getIcon("/images/openapi-generator.png")
+        private val dark = IconLoader.getIcon("/images/openapi-generator_dark.png")
+    }
+}
